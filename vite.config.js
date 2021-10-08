015afd3143c8aacb3156b45c580195035eb1bcf5
imgonetae <thewhogame@gmail.com>
2021-10-08 19:26:28 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-const {resolve} = require('path')
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build:{
-    rollupOptions:{
-      input:{
-        main:resolve(__dirname, 'index.html'),
-      }
-    }
+  plugins: [reactRefresh()],
+  alias: {
+    '@': path.resolve(__dirname, '/src')
   }
 })
